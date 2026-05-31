7cdf788c29e3611b09f90a5deea3486b6aa05498
Krzysiek <krz.rzeszot@gmail.com>
2026-05-31 15:24:51 +0200

    world/#26 add biomes, increase map and update minimap

--- scripts/ui/minimap.gd
+++ scripts/ui/minimap.gd
@@ -3,12 +3,14 @@ extends Control
 const PADDING := 10.0
 
 var player: Node2D
-var world_rect := Rect2(-1440, -880, 2880, 1760)
+var world_rect := Rect2(-2160, -1320, 4320, 2640)
+var biome_zones: Array[Dictionary] = []
 
 
-func bind(p_player: Node2D, p_world_rect: Rect2) -> void:
+func bind(p_player: Node2D, p_world_rect: Rect2, p_biome_zones: Array[Dictionary]) -> void:
 	player = p_player
 	world_rect = p_world_rect
+	biome_zones = p_biome_zones
 	queue_redraw()
 
 
@@ -24,10 +26,26 @@ func _draw() -> void:
 	draw_rect(map_rect, Color(0.74, 0.78, 0.68, 0.9), false, 1.0)
 	draw_rect(content_rect, Color(0.11, 0.18, 0.11, 0.94), true)
 	draw_rect(content_rect, Color(0.35, 0.43, 0.32, 0.8), false, 1.0)
+	_draw_biomes(content_rect)
 	_draw_grid(content_rect)
 	_draw_resources(content_rect)
 	_draw_varnaks(content_rect)
 	_draw_player(content_rect)
+	_draw_zone_label(map_rect)
+
+
+func _draw_biomes(content_rect: Rect2) -> void:
+	for biome in biome_zones:
+		var map_points := PackedVector2Array()
+		for point_value in biome["points"]:
+			map_points.append(_world_to_map(Vector2(point_value), content_rect))
+		draw_colored_polygon(map_points, Color(biome["color"]).lerp(Color.BLACK, 0.15))
+		_draw_biome_outline(map_points)
+
+
+func _draw_biome_outline(points: PackedVector2Array) -> void:
+	for i in points.size():
+		draw_line(points[i], points[(i + 1) % points.size()], Color(0.03, 0.04, 0.03, 0.55), 1.0)
 
 
 func _draw_grid(content_rect: Rect2) -> void:
@@ -62,6 +80,22 @@ func _draw_player(content_rect: Rect2) -> void:
 	var pos := _world_to_map(player.global_position, content_rect)
 	draw_circle(pos, 4.2, Color(0.17, 0.48, 1.0))
 	draw_circle(pos, 2.0, Color.WHITE)
+
+
+func _draw_zone_label(map_rect: Rect2) -> void:
+	var label_rect := Rect2(0.0, map_rect.size.y - 24.0, map_rect.size.x, 24.0)
+	draw_rect(label_rect, Color(0.03, 0.04, 0.04, 0.88), true)
+	draw_line(label_rect.position, label_rect.position + Vector2(label_rect.size.x, 0.0), Color(0.74, 0.78, 0.68, 0.55), 1.0)
+	draw_string(get_theme_default_font(), label_rect.position + Vector2(8.0, 17.0), "Zone: %s" % _get_player_zone_name(), HORIZONTAL_ALIGNMENT_LEFT, -1.0, 13, Color(0.90, 0.92, 0.84))
+
+
+func _get_player_zone_name() -> String:
+	if not is_instance_valid(player):
+		return "Unknown"
+	for biome in biome_zones:
+		if Geometry2D.is_point_in_polygon(player.global_position, PackedVector2Array(biome["points"])):
+			return str(biome.get("name", "Unknown"))
+	return "Wilderness"
 
 
 func _world_to_map(world_position: Vector2, content_rect: Rect2) -> Vector2:
